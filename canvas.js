5c63fc6638f9b62102e83917caae66b5f434e968
Pedro Monteiro <pedrosergiomaia@gmail.com>
2021-03-28 16:39:17 -0300

    Atualizando arquivos
    correção de erros e atualização de desenho

--- canvas.js
+++ canvas.js
@@ -1,51 +1,55 @@
-const canvas = document.querySelector('canvas');
-const ctx = canvas.getContext('2d');
 
-var largura = window.innerWidth;
-var altura = window.innerHeight;
 
-canvas.setAttribute("width", largura);
-canvas.setAttribute("height", altura);
+function draw(){
+  var canvas = document.getElementById('quadro');
+  var header = document.querySelector('header');
+  
+  if (canvas.getContext){
+    var ctx = canvas.getContext('2d');
+    var inicioAltura = header.clientHeight;
+    var largura = canvas.clientWidth;
+    var altura = canvas.clientHeight;
+  
+    canvas.setAttribute("width", largura);
+    canvas.setAttribute("height", altura);
+  
+    var desenhando = false;
 
-var desenhando = false;
+    const movimento = function (evt) {
+      if (desenhando) {
+          ctx.lineTo(evt.clientX, evt.clientY-inicioAltura);
+          ctx.stroke();
+      }
+  }
+  
+    const movimentoInicio = function (evt) {
+      ctx.moveTo(evt.clientX, evt.clientY-inicioAltura);
+      console.log(evt.clientX)
+      console.log(evt.clientY)
+      desenhando = true;
+  }
+  
+    const movimentoFim = function () {
+      desenhando = false;
+  }
+    
+    canvas.onmousedown = movimentoInicio;
+    canvas.onmousemove = movimento;
+    canvas.onmouseup = movimentoFim;
 
-canvas.addEventListener('touchmove', function(event) {
-    for (var i = 0; i < event.touches.length; i++) {
-      var touch = event.touches[i];
-      //ctx.beginPath();
-      //ctx.arc(touch.pageX, touch.pageY, 2, 0, 2*Math.PI, true);
-      ctx.lineTo(touch.pageX, touch.pageY);
-      //ctx.fill();
-      ctx.stroke();
-    }
-  }, false);
-
+  
   canvas.addEventListener('touchmove', function(event) {
-    event.preventDefault();
-  }, false); 
-
-
-const movimento = function (evt) {
-    if (desenhando) {
-        ctx.lineTo(evt.clientX, evt.clientY);
+      for (var i = 0; i < event.touches.length; i++) {
+        var touch = event.touches[i];
+        ctx.lineTo(touch.pageX, touch.pageY-inicioAltura);
         ctx.stroke();
-    }
+      }
+    }, false);
+  
+    canvas.addEventListener('touchmove', function(event) {
+      event.preventDefault();
+    }, false); 
+
+  }
+
 }
-
-const movimentoInicio = function (evt) {
-    ctx.moveTo(evt.clientX, evt.clientY);
-    desenhando = true;
-}
-
-const movimentoFim = function () {
-    desenhando = false;
-}
-
-canvas.onmousedown = movimentoInicio;
-canvas.ontouchstart = movimentoInicio;
-
-canvas.onmouseup = movimentoFim;
-canvas.ontouchend = movimentoFim;
-
-canvas.onmousemove = movimento;
-canvas.ontouchmove = movimento;
